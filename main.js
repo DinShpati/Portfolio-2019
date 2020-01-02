f1e6820f5fa0098df3f22c469f4df5802a4f0a5d
Muhammed Muratovic <muratovicm05@gmail.com>
2020-01-02 07:52:31 -0500

    Section 1 is finished

---
+++ main.js
@@ -0,0 +1,7 @@
+"use strict";
+
+// Getting the users screen height for section #1
+
+const screenHeight = screen.height - 120;
+
+document.getElementById('section1').style.height = screenHeight+'px';
