29f991568323ca744384fa1c69ae0d208b82749d
Din Spataj <proendergamer03@gmail.com>
2020-01-29 09:15:51 +0300

    update

--- main.js
+++ main.js
@@ -48,3 +48,69 @@ function stickyNav() {
     navbar.classList.remove("sticky", "fade_in_show").add("");
   }
 }
+
+//Scrolling add and remove
+
+var scrollKeys = {37: 1, 38: 1, 39: 1, 40: 1};
+
+function preventDefault(e) {
+  e = e || window.event;
+  if (e.preventDefault)
+      e.preventDefault();
+  e.returnValue = false;  
+}
+
+function preventDefaultForScrollKeys(e) {
+    if (scrollKeys[e.keyCode]) {
+        preventDefault(e);
+        return false;
+    }
+}
+
+function disableScroll() {
+  if (window.addEventListener) // older FF
+      window.addEventListener('DOMMouseScroll', preventDefault, false);
+  document.addEventListener('wheel', preventDefault, {passive: false}); // Disable scrolling in Chrome
+  window.onwheel = preventDefault; // modern standard
+  window.onmousewheel = document.onmousewheel = preventDefault; // older browsers, IE
+  window.ontouchmove  = preventDefault; // mobile
+  document.onkeydown  = preventDefaultForScrollKeys;
+}
+
+function enableScroll() {
+    if (window.removeEventListener)
+        window.removeEventListener('DOMMouseScroll', preventDefault, false);
+    document.removeEventListener('wheel', preventDefault, {passive: false}); // Enable scrolling in Chrome
+    window.onmousewheel = document.onmousewheel = null; 
+    window.onwheel = null; 
+    window.ontouchmove = null;  
+    document.onkeydown = null;  
+}
+
+//Open and close the navigation
+function openNav() {
+  document.getElementById("nav").style.width = "100%";
+  disableScroll();
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+  document.getElementById("nav").style.width = "0";
+  enableScroll();
+}
+
+function toggleNav(){
+
+  var x = document.getElementById("nav");
+  var y = document.getElementById("iconToggle")
+
+  if (x.style.width == 0 || x.style.width == '0px') {
+    openNav();
+  } else {
+    closeNav();
+  }
+
+  y.classList.toggle("closeIcon");
+
+}
+
